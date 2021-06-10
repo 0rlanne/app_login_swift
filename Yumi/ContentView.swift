366a46b3fc7f68475d98de28bd7b5e5d02a1a215
H0xtry <71606413+Hoxtry@users.noreply.github.com>
2021-06-10 23:13:45 +0200

    Add files via upload

---
+++ Yumi/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Yumi
+//
+//  Created by Fanch Yattou Garny on 10/06/2021.
+//
+
+
+
+
+import SwiftUI
+import ScreenTime
+
+struct ContentView: View {
+    var body: some View {
+        Text("ta mere la pute")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Yumi/YumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumiApp.swift
+//  Yumi
+//
+//  Created by Fanch Yattou Garny on 10/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct YumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
